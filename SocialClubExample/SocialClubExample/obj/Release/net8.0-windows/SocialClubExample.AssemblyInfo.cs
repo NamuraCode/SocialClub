380da26f1b06c952b3bd9a686d2f0ed65248b1a9
David <naranjo.david.1999@gmail.com>
2025-08-30 23:34:07 -0500

    person class created

--- SocialClubExample/SocialClubExample/obj/Release/net8.0-windows/SocialClubExample.AssemblyInfo.cs
+++ SocialClubExample/SocialClubExample/obj/Release/net8.0-windows/SocialClubExample.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocialClubExample")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+309d08e22fcfda480e00509c7c8f925b2f04161f")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocialClubExample")]
