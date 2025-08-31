2ec0e5db788bfa3a5125ea7c146a120ca6959576
David <naranjo.david.1999@gmail.com>
2025-08-30 23:34:07 -0500

    person class created

---
+++ SocialClubExample/SocialClubExample/domain/model/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialClubExample.domain.model
+{
+    internal class Person
+    {
+        private ulong id;
+        private string name;
+        private int age;
+        private long cellphone;
+        private long document;
+
+    }
+}
